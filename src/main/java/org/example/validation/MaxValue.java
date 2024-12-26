bb0360a7f50e82d1d4975a96a3f7736e0b09428e
ppvictoriaaa <pekurvika@gmail.com>
2024-12-27 01:00:58 +0200

    added files

---
+++ src/main/java/org/example/validation/MaxValue.java
@@ -0,0 +1,21 @@
+package org.example.validation;
+
+import java.lang.annotation.*;
+
+/**
+ * Specifies the maximum allowed value for a field.
+ *
+ * This annotation can be used to enforce that the annotated field's value is less than or equal
+ * to the specified maximum value. The annotation is retained at runtime for reflection-based validation.
+ *
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.RetentionPolicy
+ * @see java.lang.annotation.ElementType
+ * @see java.lang.annotation.Target
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface MaxValue {
+    int value();
+}
+
---
+++ src/main/java/org/example/validation/NotNull.java
@@ -0,0 +1,19 @@
+package org.example.validation;
+
+import java.lang.annotation.*;
+
+/**
+ * Indicates that a field must not be null.
+ *
+ * This annotation can be used for validation purposes to ensure that the annotated field is not null.
+ * It is retained at runtime and can be accessed via reflection.
+ *
+ * @see java.lang.annotation.Retention
+ * @see java.lang.annotation.RetentionPolicy
+ * @see java.lang.annotation.ElementType
+ * @see java.lang.annotation.Target
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface NotNull {}
+
